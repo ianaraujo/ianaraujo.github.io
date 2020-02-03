8862569f504a3a8415f2cba56c6886c0aa796439
ianaraujo <ianaraujo15@gmail.com>
2020-02-03 16:20:13 -0300

    commit master

--- app.js
+++ app.js
@@ -13,3 +13,23 @@ const navSlide = () => {
 };
 
 navSlide();
+
+function populate(s1, s2){
+  var s1  = document.getElementById(s1);
+  var s2  = document.getElementById(s2);
+  s2.innerHTML = "";
+  if(s1.value == "PT"){
+    var optionArray = ["escolha|Escolha um vereador", "leandro lyra | Leandro Lyra"];
+
+  } else if(s1.value == "DEM"){
+    var optionArray = ["escolha|Escolha um vereador", "carlos bolsonaro | Carlos Bolsonaro"];
+
+  }
+  for(var option in optionArray){
+    var pair = optionArray[option] .split("|");
+    var newOption = document.createElement("option");
+    newOption.value = pair[0];
+    newOption.innerHTML = pair[1];
+    s2.options.add(newOption);
+  }
+}
